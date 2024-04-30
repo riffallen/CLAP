e3b2569df93c08fa731713db093463b58d0d646e
riffallen <131701403+riffallen@users.noreply.github.com>
2024-04-29 22:14:22 -0700

    remove transformers version check

--- src/laion_clap/clap_module/factory.py
+++ src/laion_clap/clap_module/factory.py
@@ -61,8 +61,8 @@ def load_state_dict(checkpoint_path: str, map_location="cpu", skip_params=True):
             state_dict = {k[7:]: v for k, v in state_dict.items()}
         
         # removing position_ids to maintain compatibility with latest transformers update        
-        if version.parse(transformers.__version__) >= version.parse("4.31.0"): 
-            del state_dict["text_branch.embeddings.position_ids"]
+        # if version.parse(transformers.__version__) >= version.parse("4.31.0"): 
+        #     del state_dict["text_branch.embeddings.position_ids"]
     # for k in state_dict:
     #     if k.startswith('transformer'):
     #         v = state_dict.pop(k)
